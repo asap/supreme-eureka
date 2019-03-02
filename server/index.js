6b1df33c38ec283a01936b0b1ade4b69d2ffb1aa
Alexander Sapountzis <alex@stashable.com>
2019-03-02 07:33:51 -0500

    revise build process

--- server/index.js
+++ server/index.js
@@ -1,10 +1,5 @@
 const app = require('./app');
 const port = 5000;
-// const port = 3001;
-
-// app.get('/', (req, res) => {
-//   res.send(`PORT ${port}`);
-// });
 
 app.listen(port, err => {
   if (err) {
